e339ceb0c8eaf923543985169b54f013dac8e98b
heygourab <gourabblg901@gmail.com>
2021-11-26 00:35:52 +0530

    hashmap

--- separate_chaining.py
+++ separate_chaining.py
@@ -10,24 +10,26 @@
 
 class HashTable:
     def __init__(self, ):
-        self.array = [[] for i in range(100)]
+        self.array = [[] for i in range(10)]
 
     def hash(self, key):
         store = 0
         for i in key:
             store += ord(i)
-        return store % 100
+        return store % 10
 
     def __setitem__(self, key, value):
         h = self.hash(key)
         found = False
+        # update the value thats exists
         for idx, element in enumerate(self.array[h]):
             if len(element) == 2 and element[0] == key:
                 self.array[h][idx] = [key, value]
                 found = True
                 break
         if found == False:
-            self.array[h] = [[key, value]]
+            # add new value to the hash array
+            self.array[h].append([key, value])
 
     def __getitem__(self, key):
         h = self.hash(key)
@@ -46,8 +48,9 @@ class HashTable:
 if __name__ == '__main__':
     hm = HashTable()
     hm['gourab'] = 69
-    print(hm.array)
+    hm['return'] = 108
+    hm['hash'] = 10
+    hm['print'] = 19
+    hm['prokash'] = 70
     print(hm['gourab'])
-    del hm['gourab']
     print(hm.array)
-    print(hm['gourab'])
